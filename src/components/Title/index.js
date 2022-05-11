ac94f5b18a7c0ff613eb3766c7e7b67b42f01f60
Felipe Silveira <felipelarson@gmail.com>
2022-05-11 16:10:02 +0100

    feat: created component title

---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles.css"
+
+export const Title = () => {
+  return (
+    <div className='title'>
+      Tic
+      <span>Tac</span>
+      Toe
+    </div>
+  )
+}
