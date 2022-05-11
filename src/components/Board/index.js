c1500dedeb9a3a143462d353f77574baf29dedea
Felipe Silveira <felipelarson@gmail.com>
2022-05-11 16:12:39 +0100

    refactor: refactor code to crated array square

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,23 +3,19 @@ import "./styles.css"
 
 export const Board = () => {
 
-  const renderSquare = (position) => (
-    <Square value="X"
-      onClick={() => this.onClick(position)}
-    />
-  )
+  const squarePositions = [...Array(9).keys()]
 
   return (
     <div className="board">
-      {renderSquare(0)}
-      {renderSquare(1)}
-      {renderSquare(2)}
-      {renderSquare(3)}
-      {renderSquare(4)}
-      {renderSquare(5)}
-      {renderSquare(6)}
-      {renderSquare(7)}
-      {renderSquare(8)}
+      {squarePositions.map(index =>
+      (
+        <Square
+          key={index}
+          value=""
+          onClick={() => console.log(index)}
+        />
+      )
+      )}
     </div>
   )
 }
