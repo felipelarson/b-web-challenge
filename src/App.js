21edc2dda32d6f03e18ef850250b82ccc577b8c2
Felipe Silveira <felipelarson@gmail.com>
2022-05-11 17:43:16 +0100

    feat: created page home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles/reset.css"
 import "./styles/colors.css"
+import "./styles/fonts.css"
 import { Routes } from "./routes";
 
 export const App = () => (
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,19 @@
+import { Title } from "../../components/Title"
+import "./styles.css"
+import Img from "../../assets/img/Ilustra.png"
+import { Button } from "../../components/Button"
+
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="home-left">
+        <div>
+          <Title />
+          <p>Divirta-se nesse jogo</p>
+        </div>
+        <Button path="game" color="secondary">Iniciar</Button>
+      </div>
+      <img src={Img} alt="Ilustration" />
+    </div>
   )
 }
