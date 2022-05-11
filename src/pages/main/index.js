b5477c35ca6897e571831597a9d8d853e9b38792
Felipe Silveira <felipelarson@gmail.com>
2022-05-11 14:02:28 +0100

    feat: created page to main

---
+++ src/pages/main/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Board } from '../../components/Board'
+import "./styles.css"
+
+export const Main = () => {
+  return (
+    <div className='main'>
+      <Board />
+    </div>
+  )
+}
