9818ffd020dd0b0ea3e018abe0bf8a43035dc373
Felipe Silveira <felipelarson@gmail.com>
2022-05-11 14:02:07 +0100

    style: implement style to board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,31 +1,25 @@
 import { Square } from "../Square"
+import "./styles.css"
 
-export const Board = (props) => {
+export const Board = () => {
 
   const renderSquare = (position) => (
-    <Square value={props.squares[position]}
-      onClick={() => props.onClick(position)}
+    <Square value="X"
+      onClick={() => this.onClick(position)}
     />
   )
 
   return (
-    <div>
-      <div className="border-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="border-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="border-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-
+    <div className="board">
+      {renderSquare(0)}
+      {renderSquare(1)}
+      {renderSquare(2)}
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
     </div>
   )
 }
