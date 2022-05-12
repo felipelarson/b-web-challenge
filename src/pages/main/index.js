b026311e81af205ea274a0a7f2610e7cb14b2b20
Felipe Silveira <felipelarson@gmail.com>
2022-05-12 21:03:17 +0100

    feat: implement button to set array

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Board } from '../../components/Board'
 import { Button } from '../../components/Button'
 import { Title } from '../../components/Title'
 import "./styles.css"
 
 export const Main = () => {
+
+  const [squarePositions, setSquarePositions] = useState(["", "", "", "", "", "", "", "", "",])
+
   return (
     <div className='main'>
       <Title />
       <div className='board-main'>
-        <Board />
+        <Board squarePositions={squarePositions} />
         <div className='btn-main'>
-          <Button path="/" color="secondary">Reiniciar</Button>
+          <Button path="" color="secondary" onClick={() => setSquarePositions(["", "", "", "", "", "", "", "", "",])}>Reiniciar</Button>
           <Button path="/" color="light-primary">Voltar</Button>
         </div>
       </div>
