d01bc4983cdd58ed50b27697fe4836f9b543b74e
Felipe Silveira <felipelarson@gmail.com>
2022-05-12 21:02:47 +0100

    feat: implemented rest to button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "./style.css"
 
-export const Button = ({ children, color, path }) => {
+export const Button = ({ children, color, path, ...rest }) => {
   return (
-    <Link to={path} className='btn' style={{ background: `var(--${color})` }}>
+    <Link to={path} className='btn' style={{ background: `var(--${color})` }} {...rest}>
       {children}
     </Link >
   )
